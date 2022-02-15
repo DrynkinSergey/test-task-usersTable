f58804b77cb9b46e4a64e60580448e3a9960f6c2
Sergey <sergsmile29@gmail.com>
2022-02-15 15:28:54 +0200

    added sort by id, age, added axios get status, added avatar img  = user.age. Added save and load from localstorage

--- src/Components/Common/Button.jsx
+++ src/Components/Common/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import '../../index.css'
 const Btn = (props) => {
-    return <div className='btn'><Button onClick={props.onClick} variant="contained">Добавить пользователя</Button></div>
+    return <div className='btn'><Button onClick={props.onClick} variant="contained">{props.title}</Button></div>
     }
 
 export default Btn;
--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -9,71 +9,90 @@ import Paper from '@mui/material/Paper';
 import '../../index.css'
 import Btn from "../Common/Button";
 import {useState} from "react";
-function createData(id, avatar, name, age, status, options) {
+import axios from "axios";
+/*function createData(id, avatar, name, age, status, options) {
     return { id, avatar, name, age, status, options};
-}
+}*/
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 export default function BasicTable() {
-
-
+    const data = JSON.parse(localStorage.getItem('data'));
+    const avatarSrc =`https://avatars.dicebear.com/api/human/2.svg`
     const [state, changeState] = useState(
-        [
-            {id: 1, avatar: '-', name: 'Sergey', age: 77, status: 'Активен'},
-            {id: 2, avatar: '-', name: 'Sergey', age: 7, status: 'Активен'},
-            {id: 3, avatar: '-', name: 'Sergey', age: 17, status: 'Активен'},
-            {id: 4, avatar: '-', name: 'Sergey', age: 47, status: 'Активен'},
+
+        localStorage.getItem('data')? data : [
+            {id: 1, avatar: avatarSrc, name: 'Sergey', age: 77, status: 'Активен'},
+
         ]
     );
 
     const sortItemByAge = () => {
         const arr =(state).concat().sort((a, b) => a.age > b.age ? 1 : -1)
-            .map((item, i) =>  item);
+            .map((item) =>  item);
         changeState(arr);
     }
     const sortItemById = () => {
         const arr =(state).concat().sort((a, b) => a.id > b.id ? 1 : -1)
-            .map((item, i) =>  item);
+            .map((item) =>  item);
         changeState(arr);
     }
     const unsorted = () => {
         changeState(state)
     }
-    const addedUser = ()=> {
-        changeState( [...state, {id: state.length+1, avatar: '-', name: 'Sergey', age: 17, status: 'Активен'}])
+    const addedUser = async ()=> {
+        let status;
+       await axios.get(`https://yesno.wtf/api`)
+            .then(res => {
+                status =res.data.answer;
 
-
+            })
+        const ageRandom = getRandomInt(30);
+        const avatarSrc =`https://avatars.dicebear.com/api/male/${ageRandom}.svg`
+        console.log(avatarSrc);
+        changeState( [...state, {id: state.length+1, avatar: avatarSrc, name: 'Sergey', age:ageRandom , status: status}])
     }
+
+    function saveData ()  {
+        localStorage.setItem("data", JSON.stringify(state));
+    }
+
+    function deleteData() {
+        localStorage.removeItem('data');
+    }
+
 
     return (
         <div>
-            <Btn onClick={addedUser}/>
+            <Btn onClick={addedUser} title='Добавить пользователя'/>
+            <Btn onClick={saveData} title='Сохранить в LocalStorage'/>
+            <Btn onClick={deleteData} title='Очистить LocalStorage'/>
+
         <TableContainer component={Paper} className='table'>
             <Table sx={{ minWidth: 650 }} aria-label="test table">
                 <TableHead>
 
                     <TableRow>
-                        <TableCell  onClick={sortItemById} >№ п/п</TableCell>
-                        <TableCell align="right">Аватар</TableCell>
-                        <TableCell  align="right" onClick={unsorted}>Имя</TableCell>
+                        <TableCell onClick={sortItemById} >№ п/п</TableCell>
+                        <TableCell align="right">Аватар  </TableCell>
+                        <TableCell align="right" onClick={unsorted}>Имя</TableCell>
                         <TableCell align="right" onClick={sortItemByAge}>Возраст</TableCell>
-                        <TableCell align="right">Статус</TableCell>
-                        <TableCell align="right" onClick={addedUser}>Опции</TableCell>
+                        <TableCell align="right" >Статус</TableCell>
+                        <TableCell align="right" >Опции</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {state.map((row,index) => (
+                    {state.map((row) => (
                         <TableRow
                             key={`${row.id}_${row.index}`}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
                             <TableCell component="th" scope="row" width="20px">{row.id}</TableCell>
-                            <TableCell align="right">{row.avatar}</TableCell>
+                            <TableCell align="right"><img width='30px' src={row.avatar} alt=""/></TableCell>
                             <TableCell className='name' align="right">{row.name}</TableCell>
                             <TableCell align="right">{row.age}</TableCell>
-                            <TableCell align="right">{row.status}</TableCell>
-                            <TableCell align="right">btn</TableCell>
+                            <TableCell align="right">{row.status === 'yes'? 'Активен' : '-'}</TableCell>
+                            <TableCell align="center">btn</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
